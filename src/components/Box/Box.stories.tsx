7bef973b57c41f8556205950786484084c6fb99e
ksmfou98 <ksmfou98@naver.com>
2023-05-15 22:19:37 +0900

    feat: Box 컴포넌트 스토리 작성

--- src/components/Box/Box.stories.tsx
+++ src/components/Box/Box.stories.tsx
@@ -1,5 +1,6 @@
 import { StoryObj } from "@storybook/react";
 import { Box } from "./Box";
+import { css } from "@emotion/react";
 
 export default {
   title: "Box",
@@ -8,6 +9,24 @@ export default {
 
 type Story = StoryObj<typeof Box>;
 
+const wrapper = css`
+  div {
+    border: 1px solid #999;
+  }
+`;
+
 export const Base: Story = {
   render: () => <Box>Box</Box>,
+};
+
+export const Custom: Story = {
+  render: () => (
+    <div css={wrapper}>
+      <Box m={20}>margin</Box>
+      <Box p={20}>padding</Box>
+      <Box h={50} w={100}>
+        height, width
+      </Box>
+    </div>
+  ),
 };
